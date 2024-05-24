d0651e6e86f3847df899bb421b7769b8e70f3414
Your Name <you@example.com>
2024-05-24 12:09:04 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('cesar').addEventListener('click', () => {
+        window.location.href = 'cesar.html';
+    });
+
+    document.getElementById('xavi').addEventListener('click', () => {
+        window.location.href = 'xavi.html';
+    });
+
+    document.getElementById('devis').addEventListener('click', () => {
+        window.location.href = 'devis.html';
+    });
+
+    document.getElementById('comparaciones').addEventListener('click', () => {
+        window.location.href = 'comparaciones.html';
+    });
+
+    document.getElementById('title').addEventListener('click', () => {
+        window.location.href = 'index.html';
+    });
+});
