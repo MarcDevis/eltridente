f1cfcb90db1235a3ead617b896bdedfb64c53562
Your Name <you@example.com>
2024-05-27 09:05:51 +0200

    sin comparaciones

--- data.js
+++ data.js
@@ -3,7 +3,6 @@ const jugadores = {
         nombre: 'César',
         datos: [
             { jornada: 1, minutos: 90, goles: 1, tarjetas: 0, titular: true, convocado: true },
-            { jornada: 2, minutos: 90, goles: 1, tarjetas: 0, titular: true, convocado: true },
             // Agrega más datos de cada jornada aquí
         ]
     },
@@ -11,7 +10,6 @@ const jugadores = {
         nombre: 'Xavi',
         datos: [
             { jornada: 1, minutos: 85, goles: 0, tarjetas: 1, titular: true, convocado: true },
-            { jornada: 2, minutos: 40, goles: 1, tarjetas: 0, titular: true, convocado: true }
             // Agrega más datos de cada jornada aquí
         ]
     },
@@ -19,7 +17,6 @@ const jugadores = {
         nombre: 'Devis',
         datos: [
             { jornada: 1, minutos: 70, goles: 2, tarjetas: 0, titular: true, convocado: true },
-            { jornada: 2, minutos: 90, goles: 6, tarjetas: 0, titular: true, convocado: true }
             // Agrega más datos de cada jornada aquí
         ]
     }
