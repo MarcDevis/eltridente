f532248033bc0c98b88500f4c051ace96fe15fb2
Your Name <you@example.com>
2024-05-27 08:55:49 +0200

    prueba de datos

--- data.js
+++ data.js
@@ -2,21 +2,30 @@ const jugadores = {
     cesar: {
         nombre: 'César',
         datos: [
-            { jornada: 1, minutos: 90, goles: 1, tarjetas: 0, titular: true, convocado: true },
+            { jornada: 1, minutos: 90, goles: 1, tarjetas: 0, titular: true, convocado: true 
+                jornada: 2, minutos: 10, goles: 0, tarjetas: 1, titular: true, convocado: true
+                jornada: 3, minutos: 0, goles: 0, tarjetas: 0, titular: false, convocado: true
+            },
             // Agrega más datos de cada jornada aquí
         ]
     },
     xavi: {
         nombre: 'Xavi',
         datos: [
-            { jornada: 1, minutos: 85, goles: 0, tarjetas: 1, titular: true, convocado: true },
+            { jornada: 1, minutos: 85, goles: 0, tarjetas: 1, titular: true, convocado: true 
+                jornada: 2, minutos: 30, goles: 0, tarjetas: 0, titular: true, convocado: true
+                jornada: 3, minutos: 4, goles: 0, tarjetas: 0, titular: false, convocado: true
+            },
             // Agrega más datos de cada jornada aquí
         ]
     },
     devis: {
         nombre: 'Devis',
         datos: [
-            { jornada: 1, minutos: 70, goles: 2, tarjetas: 0, titular: true, convocado: true },
+            { jornada: 1, minutos: 70, goles: 2, tarjetas: 0, titular: true, convocado: true 
+                jornada: 2, minutos: 10, goles: 0, tarjetas: 1, titular: true, convocado: true
+                jornada: 3, minutos: 90, goles: 2, tarjetas: 0, titular: true, convocado: true
+            },
             // Agrega más datos de cada jornada aquí
         ]
     }
